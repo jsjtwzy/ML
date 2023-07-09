aa56e9bf7b96a2861c5082016eaac6a9ad45148b
ZiyuanW <jsjtwzy@163.com>
2023-07-09 14:26:57 +0800

    numpy added

--- geneticAlg.py
+++ geneticAlg.py
@@ -4,10 +4,11 @@ import random
 import math
 import matplotlib.pyplot as plt
 import time
+import numpy as np
 class GA(object):
 #初始化种群 生成chromosome_length大小的population_size个个体的种群
  
-    def __init__(self,population_size,chromosome_length,max_value,pc,pm):
+    def __init__(self,population_size,chromosome_length,max_value,pc,pm, epochs):
  
         self.population_size=population_size
         self.choromosome_length=chromosome_length
@@ -15,38 +16,29 @@ class GA(object):
         self.max_value=max_value
         self.pc=pc
         self.pm=pm
+        self.epochs = epochs
         # self.fitness_value=[]
  
  
  
     def species_origin(self):
-        population=[[]]
+        population=[]
         for _ in range(self.population_size):
  
-            temporary=[]
-        #染色体暂存器
-            for _ in range(self.choromosome_length):
- 
-                temporary.append(random.randint(0,1))
-            #随机产生一个染色体,由二进制数组成
- 
+            temporary = np.random.randint(0, 1, (self.choromosome_length))
             population.append(temporary)
             #将染色体添加到种群中
-        return population[1:]
+        return np.array(population)
             # 将种群返回，种群是个二维数组，个体和染色体两维
  
     #从二进制到十进制
     #编码  input:种群,染色体长度 编码过程就是将多元函数转化成一元函数的过程
     def translation(self,population):
- 
-        temporary=[]
-        for i in range(len(population)):
-            total=0
-            for j in range(self.choromosome_length):
-                total+=population[i][j]*(math.pow(2,j))
-            #从第一个基因开始，每位对2求幂，再求和
-            # 如：0101 转成十进制为：1 * 20 + 0 * 21 + 1 * 22 + 0 * 23 = 1 + 0 + 4 + 0 = 5
-            temporary.append(total)
+        
+        #make binary2decimal weights
+        weights = (2 *np.ones((self.choromosome_length))) **np.arange((self.choromosome_length))
+        temporary = population @weights.T
+        
         #一个染色体编码完成，由一个二进制数编码为一个十进制数
         return temporary
    # 返回种群中所有个体编码完成后的十进制数
@@ -58,12 +50,10 @@ class GA(object):
 #a protein realize its function according its structure
 # 目标函数相当于环境 对染色体进行筛选，这里是2*sin(x)+math.cos(x)
     def function(self,population):
-        temporary=[]
-        function1=[]
-        temporary=self.translation(population)
-        for ti in temporary:
-            x=ti*self.max_value/(math.pow(2,self.choromosome_length)-1)
-            function1.append(2*math.sin(x)+math.cos(x))
+        
+        temporary=self.max_value *self.translation(population) /(math.pow(2,self.choromosome_length)-1)
+        f = lambda x :2*x*np.sin(x)+np.cos(x)
+        function1 = f(temporary)
  
          #这里将sin(x)作为目标函数
         return function1
@@ -71,47 +61,29 @@ class GA(object):
 #定义适应度
     def fitness(self,function1):
  
-        fitness_value=[]
+        fitness_value=np.zeros(np.shape(function1))
+        fitness_value[function1 > 0] = function1[function1 > 0]
  
-        for fi in function1:
- 
-            if(fi>0):
-                temporary=fi
-            else:
-                temporary=0.0
-        # 如果适应度小于0,则定为0
- 
-            fitness_value.append(temporary)
+        #only function_value > 0 will be counted
         #将适应度添加到列表中
  
         return fitness_value
  
 #计算适应度和采用内置sum
  
-    '''def sum(self,fitness_value):
-        total=0
- 
-        for i in range(len(fitness_value)):
-            total+=fitness_value[i]
-        return total'''
  
 #计算适应度斐伯纳且列表
     def cumsum(self,fitness1):
-        for i in range(len(fitness1)-2,-1,-1):
-        # range(start,stop,[step])
-        # 倒计数
-            fitness1[i]=sum(fitness1[:i+1])
-            fitness1[-1]=1
+        fitness1 = np.cumsum(fitness1)
  
  
 #3.选择种群中个体适应度最大的个体
     def selection(self,population,fitness_value):
-        new_fitness=[]
+        
     #单个公式暂存器
         total_fitness=sum(fitness_value)
     #将所有的适应度求和
-        for i in range(len(fitness_value)):
-            new_fitness.append(fitness_value[i]/total_fitness)
+        new_fitness = fitness_value /total_fitness
     #将所有个体的适应度正则化
         self.cumsum(new_fitness)
     
@@ -119,7 +91,7 @@ class GA(object):
         population_length=pop_len=len(population)
     #求出种群长度
     #根据随机数确定哪几个能存活
-        ms=[random.random() for _ in range(pop_len)]
+        ms=np.random.rand(pop_len)
     # 产生种群个数的随机值
     # ms.sort()
     # 存活的种群排序
@@ -128,12 +100,12 @@ class GA(object):
         new_population=new_pop=population
  
     #轮盘赌方式
-        while newin<pop_len:
-              if(ms[newin]<new_fitness[fitin]):
-                 new_pop[newin]=population[fitin]
-                 newin+=1
+        while newin<pop_len and fitin < pop_len:
+              if ms[newin]<new_fitness[fitin]:
+                new_pop[newin]=population[fitin]
+                newin+=1
               else:
-                 fitin+=1
+                fitin+=1
         population=new_pop
  
 #4.交叉操作
@@ -147,20 +119,10 @@ class GA(object):
  
                cpoint=random.randint(0,len(population[0]))
            #在种群个数内随机生成单点交叉点
-               temporary1=[]
-               temporary2=[]
+               temporary1 = population[i][0:cpoint]
  
-               temporary1.extend(population[i][0:cpoint])
-               temporary1.extend(population[i+1][cpoint:len(population[i])])
-           #将tmporary1作为暂存器，暂时存放第i个染色体中的前0到cpoint个基因，
-           #然后再把第i+1个染色体中的后cpoint到第i个染色体中的基因个数，补充到temporary2后面
- 
-               temporary2.extend(population[i+1][0:cpoint])
-               temporary2.extend(population[i][cpoint:len(population[i])])
-        # 将tmporary2作为暂存器，暂时存放第i+1个染色体中的前0到cpoint个基因，
-        # 然后再把第i个染色体中的后cpoint到第i个染色体中的基因个数，补充到temporary2后面
-               population[i]=temporary1
-               population[i+1]=temporary2
+               population[i][0:cpoint]=population[i+1][0:cpoint]
+               population[i+1][0:cpoint]=temporary1
         # 第i个染色体和第i+1个染色体基因重组/交叉完成
     def mutation(self,population):
      # pm是概率阈值
@@ -172,19 +134,14 @@ class GA(object):
              if(random.random()<self.pm):
                 mpoint=random.randint(0,py-1)
             #
-                if(population[i][mpoint]==1):
-               #将mpoint个基因进行单点随机变异，变为0或者1
-                   population[i][mpoint]=0
-                else:
-                   population[i][mpoint]=1
+                population[i][mpoint] = abs(1 - population[i][mpoint])
  
 #transform the binary to decimalism
 # 将每一个染色体都转化成十进制 max_value,再筛去过大的值
     def b2d(self,best_individual):
-        total=0
-        b=len(best_individual)
-        for i in range(b):
-            total=total+best_individual[i]*math.pow(2,i)
+        
+        weights = (2 *np.ones((self.choromosome_length))) **np.arange((self.choromosome_length))
+        total = best_individual @weights.T
  
         total=total*self.max_value/(math.pow(2,self.choromosome_length)-1)
         return total
@@ -194,16 +151,9 @@ class GA(object):
     def best(self,population,fitness_value):
  
         px=len(population)
-        bestindividual=[]
-        bestfitness=fitness_value[0]
+        bestfitness=max(fitness_value)
+        bestindividual=population[fitness_value == bestfitness][0]
         # print(fitness_value)
- 
-        for i in range(1,px):
-   # 循环找出最大的适应度，适应度最大的也就是最好的个体
-            if(fitness_value[i]>bestfitness):
- 
-               bestfitness=fitness_value[i]
-               bestindividual=population[i]
  
         return [bestindividual,bestfitness]
  
@@ -212,7 +162,7 @@ class GA(object):
         X = []
         Y = []
  
-        for i in range(500):
+        for i in range(self.epochs):
             X.append(i)
             Y.append(results[i][0])
  
@@ -228,7 +178,7 @@ class GA(object):
  
         population = pop = self.species_origin()
  
-        for i in range(500):
+        for i in range(self.epochs):
             function_value = self.function(population)
             # print('fit funtion_value:',function_value)
             fitness_value = self.fitness(function_value)
@@ -253,5 +203,5 @@ if __name__ == '__main__':
    chromosome_length=20
    pc=0.6
    pm=0.01
-   ga=GA(population_size,chromosome_length,max_value,pc,pm)
+   ga=GA(population_size,chromosome_length,max_value,pc,pm, 30)
    ga.main()
