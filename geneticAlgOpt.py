cfb61861fb50c55531a81e3b3ddd7bba185da36b
ZiyuanW <jsjtwzy@outlook.com>
2023-07-25 20:31:17 +0800

    modded

--- geneticAlgOpt.py
+++ geneticAlgOpt.py
@@ -3,26 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+class TagetFunc():
+    def __init__(self, chrom_len, max_value=1, min_value=0):
+        self.max_value = max_value
+        self.min_value = min_value
+        self.chrom_len = chrom_len
 
-def translation(p):
-    
-    max_value = 10
-    #make binary2decimal weights
-    weights = max_value *(2 *np.ones((len(p)))) **np.arange((len(p))) /(math.pow(2,len(p))-1)
-    temporary = p @weights.T
+    def translation(self, p):
         
-    #一个染色体编码完成，由一个二进制数编码为一个十进制数
-    return temporary
-def function(p):
-    x = translation(p)
-    print(x)
-    return 2 *np.sin(x) +np.cos(x)
+        #make binary2decimal weights
+        weights = self.max_value *(2 *np.ones((len(p)))) **np.arange((len(p))) /(math.pow(2,len(p))-1)
+        temporary = np.array(p >0.5).astype(int) @weights.T
+        
+        #一个染色体编码完成，由一个二进制数编码为一个十进制数
+        return temporary
+
+    def function(self, p):
+        x = self.translation(p)
+        return 2 *np.sin(x) +np.cos(x)
 
 
-chromosome_length=20
+taget = TagetFunc(20, max_value=10)
+chrom_len = taget.chrom_len
+func = taget.function
 # ndim can't be 1 ,so that crossover vill not be done
-ga = GA(func=function, n_dim=chromosome_length, size_pop=40, max_iter=50, prob_mut=0.01, ub=1, lb=0)
+ga = GA(func=func, n_dim=chrom_len, size_pop=40, max_iter=50, prob_mut=0.01, ub=1, lb=0)
 ga.run()
+best_x = np.array(ga.best_x >0.5).astype(int)
+print(best_x)
 his_y = np.array(ga.all_history_FitV)
 max_y = np.max(his_y,1)
 plot1 = plt.plot(max_y)
